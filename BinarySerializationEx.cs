168d98f1a1094d39c955688aab9e9280f3d51c79
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-01 19:25:45 +0530

    first commit

--- BinarySerializationEx.cs
+++ BinarySerializationEx.cs
@@ -1,52 +1,31 @@
 ﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
 namespace BinarySerializationDemo
 {
-    class BinarySerializtionOpertaions
+    [Serializable] //serializing an object to binary
+    public class Employee
     {
-        //const string filename = "AppSettings.dat";
-        public void WriteDefaultValue()
-        {
-            using (BinaryWriter writer = new BinaryWriter(File.Open(@"D:\wizzie\DOT NET\BinarySerialization.txt", FileMode.Create)))
-            {
-                writer.Write(1.250F);
-                writer.Write(@"D:\wizzie\DOT NET");
-                writer.Write(10);
-                writer.Write(true);
-            }
-        }
-        public void DisplayValue()
-        {
-            float aspectRatio;
-            string tempDirectory;
-            int autoSaveTime;
-            bool showStatusBar;
-
-            if (File.Exists(@"D:\wizzie\DOT NET\BinarySerialization.txt"))
-            {
-                using (BinaryReader reader = new BinaryReader(File.Open(@"D:\wizzie\DOT NET\BinarySerialization.txt", FileMode.Open)))
-                {
-                    aspectRatio = reader.ReadSingle();
-                    tempDirectory = reader.ReadString();
-                    autoSaveTime = reader.ReadInt32();
-                    showStatusBar = reader.ReadBoolean();
-                }
-
-                Console.WriteLine("Aspect ratio set to: " + aspectRatio);
-                Console.WriteLine("Temp directory is: " + tempDirectory);
-                Console.WriteLine("Auto save time set to: " + autoSaveTime);
-                Console.WriteLine("Show status bar: " + showStatusBar);
-            }
-        }
+        public string EmployeeName { get; set; } = "Vivek Jadhav";
+        public int EmployeeId { get; set; } = 1001;
     }
+    
     class BinarySerializationEx
     {
         static void Main(string[] args)
         {
-            BinarySerializtionOpertaions BSO = new BinarySerializtionOpertaions();
-            BSO.WriteDefaultValue();
-            BSO.DisplayValue();
+            Employee emp = new Employee();
+            FileStream fileStream = new FileStream(@"D:\wizzie\DOT NET\Questions1.txt", FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fileStream, emp);
+            //Console.WriteLine(formatter.Serialize(fileStream, emp));
+
+            Employee deemp = new Employee();
+            FileStream fileStream1 = new FileStream(@"D:\wizzie\DOT NET\Questions1.txt", FileMode.Create);
+            BinaryFormatter formatter1 = new BinaryFormatter();
+            formatter.Serialize(fileStream1, deemp);
+
         }
     }
 }
